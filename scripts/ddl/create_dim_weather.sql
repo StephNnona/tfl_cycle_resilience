1803e67a14a89c1cc77255f8a9bdc1363f2105d0
Stephanie Nnona <stephanie.ngozichukwuka@gmail.com>
2026-02-24 23:20:40 +0000

    Updated dim_weather table with rain and temperature categories

--- scripts/ddl/create_dim_weather.sql
+++ scripts/ddl/create_dim_weather.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE TABLE `tfl-efficiency-project.tfl_efficiency_us.dim_weather` AS
-SELECT 
+WITH weather_stats AS (
+    SELECT 
     date AS weather_date,
     ROUND(AVG(CASE
         WHEN temp = 9999.9 THEN NULL  -- Handle missing temperature values (9999.9) by setting them to NULL
@@ -15,4 +16,32 @@ SELECT
     END), 1) AS wind_speed_mph
 FROM `bigquery-public-data.noaa_gsod.gsod2025`
 WHERE stn IN ('037720', '037700', '037690', '037810', '037760')  -- Covers Greater London stations
-GROUP BY weather_date;
+GROUP BY weather_date
+)
+SELECT *,
+    CASE
+        WHEN rain_mm = 0 THEN "No Rain"
+        WHEN rain_mm <= 2 THEN "Light Rain"
+        ELSE "Heavy Rain"
+    END AS rain_category,
+    CASE
+        WHEN avg_temp_celsius < 5 THEN "Cold"
+        WHEN avg_temp_celsius <20 THEN "Mild"
+        WHEN avg_temp_celsius <28 THEN "Warm"
+        ELSE "Hot"
+    END AS temperature_category,
+    CASE                -- Binary indicator for heavy rain created for resilience analysis
+        WHEN rain_mm > 2 THEN 1
+        ELSE 0
+    END AS is_heavy_rain_indicator,
+    CASE                -- Binary indicator for hot day created for resilience analysis
+        WHEN avg_temp_celsius > 28 THEN 1 
+        ELSE 0 
+    END AS is_hot_day_indicator,
+    CASE 
+        WHEN rain_mm > 2 AND avg_temp_celsius > 28 THEN "Severe Weather"  -- Define severe weather as heavy rain and hot temperature
+        WHEN rain_mm > 2 THEN "Heavy Rain Only"
+        WHEN avg_temp_celsius > 28 THEN "Hot Day Only"
+        ELSE "Normal Weather"
+    END AS weather_severity_level
+FROM weather_stats;
