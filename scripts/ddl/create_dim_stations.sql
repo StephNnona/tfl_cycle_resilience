e7899e355cf9b2f2995aa609270ee607afa749f5
Stephanie Nnona <stephanie.ngozichukwuka@gmail.com>
2026-02-24 19:44:34 +0000

    Rebuilt dim_stations from raw cycle data due to missing station IDs in initial creation

--- scripts/ddl/create_dim_stations.sql
+++ scripts/ddl/create_dim_stations.sql
@@ -1,8 +1,17 @@
-CREATE OR REPLACE TABLE `tfl-efficiency-project.tfl_efficiency_eu.dim_stations` AS
-SELECT 
-    id AS station_id,
-    name AS station_name,
-    latitude,
-    longitude,
-    docks_count
-FROM `bigquery-public-data.london_bicycles.cycle_stations`;
+CREATE OR REPLACE TABLE `tfl-efficiency-project.tfl_efficiency_us.dim_stations` AS
+WITH station_raw AS (
+    SELECT
+        CAST(start_station_id AS INT64) AS station_id,
+        start_station_name AS station_name
+    FROM `tfl-efficiency-project.tfl_raw.cycle_trips_2025`
+    UNION ALL
+    SELECT
+        CAST(end_station_id AS INT64) AS station_id,
+        end_station_name AS station_name
+    FROM `tfl-efficiency-project.tfl_raw.cycle_trips_2025`
+    WHERE end_station_id IS NOT NULL
+)
+SELECT DISTINCT
+    station_id,
+    station_name
+FROM station_raw;
